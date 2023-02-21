a218d28bb8859c358b75973a6fa22ccde770231b
lcswnn <118494460+lcswnn@users.noreply.github.com>
2023-02-21 13:23:54 -0600

    Added StateInfo.java file
    This file was used as a DATABASE file that kept tract of the number of blood units per state

---
+++ StateInfo.java
@@ -0,0 +1,68 @@
+
+public class StateInfo {
+	
+	static String[] States = {"Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", 
+			"Delaware", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", 
+			"Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", 
+			"Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina",
+			"North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", 
+			"Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"};
+	
+	//10
+	static int AlabamaNumber = 270000000;
+	static int AlaskaNumber = 174000000;
+	static int ArizonaNumber = 252000000;
+	static int ArkansasNumber = 272000000;
+	static int CaliforniaNumber = 272000000;
+	static int ColoradoNumber = 272000000;
+	static int ConnecticutNumber = 292000000;
+	static int DelawareNumber = 272000000;
+	static int FloridaNumber = 372000000;
+	static int GeorgiaNumber = 272000000;
+	//20
+	static int HawaiiNumber = 172000000;
+	static int IdahoNumber = 272000000;
+	static int IllinoisNumber = 472000000;
+	static int IndianaNumber = 272000000;
+	static int IowaNumber = 272000000;
+	static int KansasNumber = 172000000;
+	static int KentuckyNumber = 272000000;
+	static int LouisianaNumber = 272000000;
+	static int MaineNumber = 222000000;
+	static int MarylandNumber = 320000000;
+	//30
+	static int MassachusettsNumber = 234000000;
+	static int MichiganNumber = 312000000;
+	static int MinnesotaNumber = 272000000;
+	static int MississippiNumber = 272000000;
+	static int MissouriNumber = 272000000;
+	static int MontanaNumber = 272000000;
+	static int NebraskaNumber = 272000000;
+	static int NevadaNumber = 261000000;
+	static int NewHampshireNumber = 262000000;
+	static int NewJerseyNumber = 283000000;
+	//40
+	static int NewMexicoNumber = 72000000;
+	static int NewYorkNumber = 272000000;
+	static int NorthCarolinaNumber = 272000000;
+	static int NorthDakotaNumber = 272000000;
+	static int OhioNumber = 272000000;
+	static int OklahomaNumber = 282000000;
+	static int OregonNumber = 272000000;
+	static int PennsylvaniaNumber = 272000000;
+	static int RhodeIslandNumber = 672000000;
+	static int SouthCarolinaNumber = 72000000;
+	//50
+	static int SouthDakotaNumber = 342000000;
+	static int TennesseeNumber = 274000000;
+	static int TexasNumber = 272000000;
+	static int UtahNumber = 302000000;
+	static int VermontNumber = 202000000;
+	static int VirginiaNumber = 342000000;
+	static int WashingtonNumber = 272000000;
+	static int WestVirginiaNumber = 272000000;
+	static int WisconsinNumber = 172000000;
+	static int WyomingNumber = 27000000;
+	
+	
+}
